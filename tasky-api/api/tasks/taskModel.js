87e83a27c174ddde1cfb5af1c4aac277d51907f5
Mohannad Khrata <20096866@mail.wit.ie>
2025-08-29 19:32:46 +0100

    Added userId to tasks

--- tasky-api/api/tasks/taskModel.js
+++ tasky-api/api/tasks/taskModel.js
@@ -3,25 +3,22 @@ import mongoose from 'mongoose';
 const Schema = mongoose.Schema;
 
 const TaskSchema = new Schema({
-    title: { type: String, required: true },
-  description:  String ,
+  title: { type: String, required: true },
+  description: String,
   deadline: Date,
   done: Boolean,
-  // accept "low", "medium", "high" as well as "Low", "Medium", "High"
   priority: { type: String, enum: ["Low", "Medium", "High"], required: true },
   created_at: Date,
-  updated_at: Date
+  updated_at: Date,
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'User'
+  }
 });
 
-
-
 const dateValidator = (date) => {
-    return date > new Date();
-  }
-  TaskSchema.path("deadline").validate(dateValidator);
-  
-
-
-
+  return date > new Date();
+}
+TaskSchema.path("deadline").validate(dateValidator);
 
 export default mongoose.model('Task', TaskSchema);
